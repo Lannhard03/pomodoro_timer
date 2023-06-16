70835d264191ad3f8985a66fcc8156fc7a69f368
Gustav Lannhard <91192964+Lannhard03@users.noreply.github.com>
2023-06-16 12:05:52 +0200

    Update sw.js

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './pomodoro_timer.js',
+  './pomodoro_timer_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
